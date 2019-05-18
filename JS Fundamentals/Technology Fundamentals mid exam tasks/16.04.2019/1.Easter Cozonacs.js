9028544cd4e37bace1e9182d721a5025a1e852e3
taskoff <marian_taskov@abv.bg>
2019-05-18 14:18:35 +0300

    correction

--- JS Fundamentals/Technology Fundamentals mid exam tasks/16.04.2019/1.Easter Cozonacs.js
+++ JS Fundamentals/Technology Fundamentals mid exam tasks/16.04.2019/1.Easter Cozonacs.js
@@ -1,21 +1,23 @@
 function easterCosinacs(budget, priceForKgFlour) {
+
+    
     let priceForEgs = priceForKgFlour * 0.75;
     let priceForliterMilk = priceForKgFlour * 1.25;
     let priceForQuarterMilk = priceForliterMilk / 4;
     let priseForCosunac = priceForEgs + priceForQuarterMilk + priceForKgFlour;
     let cosunacs = 0;
-    let coloredEgs = 0;
+    let coloredEggs = 0;
 
     while (budget >= priseForCosunac ) {
         cosunacs++;
         budget -= priseForCosunac;
-        coloredEgs += 3;
+        coloredEggs += 3;
         if (cosunacs % 3 == 0) {
-            coloredEgs -= (cosunacs - 2)
+            coloredEggs -= (cosunacs - 2)
         }
     }
-    console.log(`You made ${cosunacs} cozonacs! Now you have ${coloredEgs} eggs and ${budget.toFixed(2)}BGN left.`);
+    
+    console.log(`You made ${cosunacs} cozonacs! Now you have ${coloredEggs} eggs and ${budget.toFixed(2)}BGN left.`);
     
 }
-easterCosinacs(20.50, 1.25
-    )
+easterCosinacs(15.75, 1.4 )
