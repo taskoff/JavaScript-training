835c4d1005e5c641b40a4e1065c1400fbb61cecd
taskoff <marian_taskov@abv.bg>
2019-05-20 10:09:37 +0300

    correction

--- JS Fundamentals/Technology Fundamentals mid exam tasks/16.04.2019/3.EasterShopping.js
+++ JS Fundamentals/Technology Fundamentals mid exam tasks/16.04.2019/3.EasterShopping.js
@@ -6,9 +6,6 @@ function easterShopping(arr) {
     for (let i = 0; i < countOfCommands; i++) {
         let commandAndIndexes = arr.shift().split(' ');
         let command = commandAndIndexes.shift();  
-        
-    
-    
         if (command === 'Include') {
             let shop = commandAndIndexes[0];
             shopList.push(shop);
@@ -19,36 +16,37 @@ function easterShopping(arr) {
                 if (place === 'first') {
                     shopList.splice(0, numberOfShops)
                 } else if (place === 'last') {
-                    shopList.splice((shopList.length - 1) - numberOfShops)
+                    shopList.splice(shopList.length - numberOfShops)
                 }
             }
+            //console.log(shopList)
         } else if (command === 'Prefer') {
             let indexOne = +commandAndIndexes[0];
             let indexTwo = +commandAndIndexes[1];
-            if ((0 <= indexOne && indexOne <= shopList.length - 1) && (0 <= indexTwo && indexTwo <= shopList.length - 1) ) {
+            if ((0 <= indexOne && indexOne <= shopList.length -1) && (0 <= indexTwo && indexTwo <= shopList.length - 1) ) {
                 let shop = shopList[indexOne];
                 shopList[indexOne] = shopList[indexTwo];
                 shopList[indexTwo] = shop;
                 
             }
+            //console.log(shopList)
         } else if (command === 'Place') {
             let shop = commandAndIndexes[0];
             let index = +commandAndIndexes[1];
-            if (0 <= (index + 1) < shopList.length) {
-                shopList.splice(index +1, 0, shop);
-                
-            }
+            if (0 <= (index + 1) && (index + 1) < shopList.length) {
+                shopList.splice(index +1, 0, shop);            }
         }
     }
-    console.log(`Shops left: ${shopList.join(' ')}`);
+    console.log(`Shops left:`);
+    console.log(`${shopList.join(' ')}`);
     
 }
-easterShopping(['Bershka CandyStore ThriftShop Armani Groceries ToyStore PeakStore',
-    5,
-    'Include HM',
-    'Visit first 2',
-    'Visit last ',
-    'Prefer 3 1',
-    'Place Library 2'
-
+easterShopping(['Boutique Flowers CandyStore ThriftShop Versace Groceries ToyStore PeakStore',
+    6,
+    'Visit first 9',
+    'Visit last 4',
+    'Prefer 3 8',
+    'Prefer 0 1',
+    'Place Store 7',
+    'Place ShoeAquarium 2'
     ])
